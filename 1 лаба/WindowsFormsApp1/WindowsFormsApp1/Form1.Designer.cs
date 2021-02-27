85814e5192f74f23614592522935e570c71958ee
Ponomarev Nikita <79397499+redirect352@users.noreply.github.com>
2021-02-27 10:29:55 +0300

    1 Лабораторная работа
    Шифрование/дешифрование методом вижера, "железнодорожной изгороди"
    и другие

---
+++ 1 лаба/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.DecrText = new System.Windows.Forms.TextBox();
+            this.EncrText = new System.Windows.Forms.TextBox();
+            this.KeyNumb = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KeyNumb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(241, 362);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(114, 51);
+            this.EncryptButton.TabIndex = 0;
+            this.EncryptButton.Text = "Зашифровать";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(427, 362);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(114, 51);
+            this.DecryptButton.TabIndex = 3;
+            this.DecryptButton.Text = "Расшифровать";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // DecrText
+            // 
+            this.DecrText.Location = new System.Drawing.Point(42, 24);
+            this.DecrText.Multiline = true;
+            this.DecrText.Name = "DecrText";
+            this.DecrText.Size = new System.Drawing.Size(313, 286);
+            this.DecrText.TabIndex = 6;
+            // 
+            // EncrText
+            // 
+            this.EncrText.Location = new System.Drawing.Point(427, 24);
+            this.EncrText.Multiline = true;
+            this.EncrText.Name = "EncrText";
+            this.EncrText.Size = new System.Drawing.Size(330, 286);
+            this.EncrText.TabIndex = 7;
+            this.EncrText.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // KeyNumb
+            // 
+            this.KeyNumb.Location = new System.Drawing.Point(637, 362);
+            this.KeyNumb.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.KeyNumb.Name = "KeyNumb";
+            this.KeyNumb.Size = new System.Drawing.Size(120, 20);
+            this.KeyNumb.TabIndex = 8;
+            this.KeyNumb.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ключ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Пидорский метод",
+            "Столбцы ",
+            "Вижер",
+            "Решетка"});
+            this.comboBox1.Location = new System.Drawing.Point(42, 334);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(637, 336);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new System.Drawing.Size(120, 20);
+            this.KeyBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.KeyBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KeyNumb);
+            this.Controls.Add(this.EncrText);
+            this.Controls.Add(this.DecrText);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Name = "Form1";
+            this.Text = "Shifrator";
+            ((System.ComponentModel.ISupportInitialize)(this.KeyNumb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.TextBox DecrText;
+        private System.Windows.Forms.TextBox EncrText;
+        private System.Windows.Forms.NumericUpDown KeyNumb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox KeyBox;
+    }
+}
+
---
+++ 1 лаба/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,723 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+
+
+            EncrText.Clear();
+
+            var Reader = new StringReader();
+            Reader.Source = DecrText;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    var R = new RailFenceEnc();
+                    R.Key = (int)KeyNumb.Value;
+                    while (Reader.ReadLine())
+                    {
+                        R.Text = Reader.LastLine;
+                        R.EncryptText();
+                        EncrText.Text = EncrText.Text + R.Text + "\r\n";
+                    }
+                    break;
+                case 1:
+                    var T = new TableEncrypt();
+                    T.Key = KeyBox.Text;
+                    if (T.Key == "")
+                        return;
+                    while (Reader.ReadLine())
+                    {
+                        T.Text = Reader.LastLine;
+                        T.EncryptText();
+                        EncrText.Text = EncrText.Text + T.Text + "\r\n";
+                    }
+                    break;
+                case 2:
+                    var V = new VigenerEncrypt();
+                    V.Key = KeyBox.Text;
+                    if (V.Key == "")
+                        return;
+                    while (Reader.ReadLine())
+                    {
+                        V.Text = Reader.LastLine;
+                        V.EncryptText();
+                        EncrText.Text = EncrText.Text + V.Text + "\r\n";
+                    }
+                    break;
+                case 3:
+                    var Rs = new ReshetkaEncrypt();
+                    while (Reader.ReadLine())
+                    {
+                        Rs.Text = Reader.LastLine;
+                        Rs.EncryptText();
+                        EncrText.Text = EncrText.Text + Rs.Text + "\r\n";
+                    }
+
+                    break;
+
+            }
+
+
+
+
+            
+           
+            
+            
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            int ind = comboBox1.SelectedIndex;
+            DecrText.Clear();
+            var Reader = new StringReader();     
+            Reader.Source = EncrText;
+
+            switch (ind) {
+                case 0:
+                    var R = new RailFenceEnc();
+                    R.Key = (int)KeyNumb.Value;
+
+                    while (Reader.ReadLine())
+                    {
+                        R.Text = Reader.LastLine;
+                        R.DecryptText();
+                        DecrText.Text = DecrText.Text + R.Text + "\r\n";
+                    }
+                    break;
+
+                case 1:
+                    var T = new TableEncrypt();
+                    T.Key = KeyBox.Text;
+                    if (T.Key == "")
+                        return;
+                    while (Reader.ReadLine())
+                    {
+                        T.Text = Reader.LastLine;
+                        T.DecryptText();
+                        DecrText.Text = DecrText.Text + T.Text + "\r\n";
+                    }
+                    break;
+                case 2:
+                    var V = new VigenerEncrypt();
+                    V.Key = KeyBox.Text;
+                    if (V.Key == "")
+                        return;
+                    while (Reader.ReadLine())
+                    {
+                        V.Text = Reader.LastLine;
+                        V.DecryptText();
+                        DecrText.Text = DecrText.Text + V.Text + "\r\n";
+                    }
+                    break;
+                case 3:
+                    var Rs = new ReshetkaEncrypt();
+                    while (Reader.ReadLine())
+                    {
+                        Rs.Text = Reader.LastLine;
+                        Rs.DecryptText();
+                        DecrText.Text = DecrText.Text + Rs.Text + "\r\n";
+                    }
+
+                    break;
+
+
+            }
+            
+
+        }
+    }
+
+
+    
+
+    public class RailFenceEnc : Object
+    {
+        private int key = 0;
+        private string text = "";
+
+        public int Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                if (value >= 0)
+                    key = value;
+                else
+                    MessageBox.Show("Invalid Key value");
+
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        public void EncryptText() {
+
+            int len = text.Length;
+            char[,]tmp = new char [key,len];
+
+            int i = 0, j = 0, ost, period = 2*(key-1); ;
+            for ( i = 0; i < len ; i++ )
+            {
+                ost = i % period;
+                j = key - 1 - Math.Abs(key - 1 - ost);              
+                tmp[j,i] = text[i];       
+            }
+            text = "";
+            for (i = 0; i < key; i++)
+                for (j = 0; j < len; j++)
+                    if (tmp[i, j] != 0)
+                        text = text + tmp[i, j];
+            
+        }
+        public void DecryptText() {
+
+            string result = "";
+            int len = text.Length;
+            char[,] tmp = new char[key, len];
+
+            int cycle = 2 * key - 2, i = 0, rowkey = key, step = cycle, row, n;
+            int char_pos, block;
+            for ( row = 0; row < key  ; row++ ) {
+                char_pos = row ;
+                n = 1;
+
+                while ( char_pos < len)
+                {
+                    
+                    if (i >= len)
+                        break;
+                    tmp[row,char_pos] = text[i];
+                    i++;
+                    if (row != 0 && row !=( key - 1))
+                    {
+                        char_pos += step;
+                        n = -n; 
+                        if (n < 0)
+                            step = row*2;
+                        else
+                            step = 2 * rowkey - 2;
+                        
+
+                    }
+                    else
+                        char_pos += step;
+
+                }
+                rowkey--;
+                if (rowkey == 1)
+                    rowkey = key;
+                step = 2 * rowkey - 2;
+            }
+
+            step = 1;
+            row = 0;
+
+            for (i = 0; i < len; i++)
+            {
+                result = result + tmp[row, i];
+                row = row + step;
+                if (row == key - 1 || row == 0)
+                    step = -step;
+            }
+
+            text = result;
+
+        }
+
+
+
+    }
+
+    public class TableEncrypt : Object
+    {
+        private string key = "";
+        private string text = "";
+        private int[] keys;
+
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        private void TurnIntoNums()
+        {
+            int len = key.Length,j,i, min;
+            char[] tmp = key.ToCharArray();
+            keys = new int[len];
+            for (i = 0; i < len; i++)
+            {
+                min = 0;
+                for (j = 1; j < len; j++)
+                {
+                    if (tmp[min] > tmp[j])
+                        min = j;
+                }
+                keys[i] = min;
+                tmp[min] = Char.MaxValue;
+
+            }
+
+        }
+
+
+        public void EncryptText()
+        {
+            int len = key.Length, rows = text.Length/len +1,i,j,k;
+            this.TurnIntoNums();
+            char[,] tmp = new char[rows, len];
+
+            k = 0;
+            for (i = 0; i < rows; i++)           
+                for (j=0; j < len; j++)
+                {
+                    if ( k < text.Length)
+                    {
+                        tmp[i, j] = text[k];
+                        k++;
+                    }
+                    else
+                        tmp[i, j] = Char.MaxValue;
+
+                }
+
+            string result = "";
+
+            for (i = 0; i < len; i++)
+            {
+                k = keys[i];
+                for (j = 0; j < rows; j++)
+                {
+                    if (tmp[j, k] != Char.MaxValue)
+                        result = result + tmp[j,k];
+                }
+            }
+            text = result;
+
+
+
+        }
+
+        public void DecryptText()
+        {
+            int len = key.Length, rows = text.Length / len + 1, i, j, k,g;
+            int LastRow = text.Length % len;
+            this.TurnIntoNums();
+            char[,] tmp = new char[rows, len];
+
+            g = 0;
+            for (i = 0; i < len; i++)
+            {
+                k = keys[i];
+                
+                for (j = 0; j < rows; j++)
+                {
+                    if (j == rows - 1 && k >=LastRow)
+                        tmp[j,k] = Char.MaxValue;
+                    else
+                    {
+                        tmp[j, k] = text[g];
+                        g++;
+                    }
+                }
+            }
+
+            text = "";
+            for (i = 0; i < rows; i++)
+                for (j = 0; j < len; j++)
+                {
+                    if (tmp[i, j] != Char.MaxValue)
+                        text = text + tmp[i,j];
+                }
+
+
+        }
+
+
+
+    }
+
+    public class ReshetkaEncrypt : Object
+    {
+        const int size = 4;
+        private string text = "";
+        private int[,] table1 = { { 1, 0, 0, 0 }, { 0, 0, 0, 1 }, { 0, 0, 1, 0 }, { 0, 1, 0, 0 } };
+        private int[,] table2 = { { 0, 0, 0, 1 }, { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 } };
+        private int[,] table3 = { { 0, 0, 1, 0 }, { 0, 1, 0, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 1 } };
+        private int[,] table4 = { { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 }, { 1, 0, 0, 0 } };
+
+
+
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        private string encTwoBytes(char[] s)
+        {
+            char[, ] tmp = new char [size, size];
+            int i,j,l=0;
+            string result = "";
+            for ( i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    if (table1[i,j] == 1) {
+                        tmp[i, j] = s[l];
+                        l++;
+                    }
+
+                }
+            }
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    if (table2[i, j] == 1)
+                    {
+                        tmp[i, j] = s[l];
+                        l++;
+                    }
+
+                }
+            }
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    if (table3[i, j] == 1)
+                    {
+                        tmp[i, j] = s[l];
+                        l++;
+                    }
+
+                }
+            }
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    if (table4[i, j] == 1)
+                    {
+                        tmp[i, j] = s[l];
+                        l++;
+                    }
+
+                }
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                   if(tmp[i,j] != 0)
+                       result = result + tmp[i, j];
+
+                }
+            }
+
+            return result;
+        }
+
+        public void EncryptText()
+        {
+
+            int S = size * size, len = text.Length;
+            string result = "";
+            char[] tmp = new char[S];
+            int k = text.Length / 16,i;
+            if (text.Length % 16 != 0)
+                k++;
+            for (i = 0; i < k; i++)
+            {
+                if (len >S)
+                    text.CopyTo(i* S, tmp,0,S);
+                else
+                    text.CopyTo(i * S, tmp, 0, len);
+                len -= S;
+                result = result + encTwoBytes(tmp);
+            }
+            text = result;
+
+        }
+
+
+
+        private string decTwoBytes(char[] s)
+        {
+            char[,] tmp = new char[size, size];
+            int i, j, l = 0;
+            string result = "";
+           
+            for (i = 0; i < size; i++)
+                for (j = 0; j < size; j++)
+                    tmp[i, j] = s[i * size + j];
+
+            for (i = 0; i < size; i++)           
+                for (j = 0; j < size; j++)              
+                    if (table1[i, j] == 1 && tmp[i,j] != 0)                   
+                        result = result + tmp[i, j];
+
+            for (i = 0; i < size; i++)
+                for (j = 0; j < size; j++)
+                    if (table2[i, j] == 1 && tmp[i, j] != 0)
+                        result = result + tmp[i, j];
+
+            for (i = 0; i < size; i++)
+                for (j = 0; j < size; j++)
+                    if (table3[i, j] == 1 && tmp[i, j] != 0)
+                        result = result + tmp[i, j];
+
+            for (i = 0; i < size; i++)
+                for (j = 0; j < size; j++)
+                    if (table4[i, j] == 1 && tmp[i, j] != 0)
+                        result = result + tmp[i, j];
+
+            return result;
+        }
+
+
+
+        public void DecryptText()
+        {
+            int S = size * size, len = text.Length;
+            string result = "";
+            char[] tmp = new char[S];
+            int k = text.Length / 16, i;
+            if (text.Length % 16 != 0)
+                k++;
+            for (i = 0; i < k; i++)
+            {
+                if (len > S)
+                    text.CopyTo(i * S, tmp, 0, S);
+                else
+                    text.CopyTo(i * S, tmp, 0, len);
+                len -= S;
+                result = result + decTwoBytes(tmp);
+            }
+            text = result;
+
+        }
+
+
+    }
+
+
+
+    public class VigenerEncrypt : Object
+    {
+        private const int AlphabetSize = 26;
+        private string key = "";
+        private string text = "";
+        private char[,] keys;
+
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+
+        private void MakeTable()
+        {
+            keys = new char[AlphabetSize,AlphabetSize];
+            char start = 'a', tmp;
+            for (int i = 0; i < AlphabetSize; i++)
+            {
+
+                tmp = start;
+                for (int j = 0; j < AlphabetSize; j++)
+                {
+                    keys[i, j] = tmp;
+                    tmp++;
+                    if (tmp > 'z')
+                        tmp = 'a';
+                }
+                start++;
+
+            }
+
+        }
+
+        public void EncryptText()
+        {
+            this.MakeTable();
+            int len = text.Length, i, a,b;
+            text =  text.ToLower();
+            string result = "";
+            for (i = 0; i < len; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    result = result + text[i];
+                    continue;
+                }
+                a = text[i] - 'a';
+                b = key[i % key.Length] - 'a';
+                result = result + keys[a,b];
+
+            }
+            text = result;
+        }
+
+        public void DecryptText()
+        {
+            this.MakeTable();
+            int len = text.Length, i, a, b;
+            string result = "";
+            for (i = 0; i < len; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    result = result + text[i];
+                    continue;
+                }
+                b = key[i % key.Length] - 'a';
+                a = 'a';
+                for (int j = 0; j < AlphabetSize; j++)
+                {
+                   
+                    if (keys[j, b] == text[i])
+                        break;
+                    a++;
+
+                }
+
+                result = result + (char)a;
+
+            }
+            text = result;
+
+        }   
+
+
+
+    }
+
+
+    public class StringReader : Object
+    {
+        private TextBox source;
+        public string LastLine = "";
+        private int i = 0;
+
+        public TextBox Source
+        {
+            set
+            {
+                i = 0;
+                source = value;
+
+            }
+        }
+
+
+        public bool ReadLine()
+        {
+            if (i >= source.Lines.Length)
+                return false;
+            else
+            {
+                LastLine = (string)source.Lines[i];
+                i++;
+                return true;
+            }
+        }
+
+
+    }
+
+
+
+
+}
